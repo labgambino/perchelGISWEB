00ddc70a164305344c0c599cf70f7d64364cbea2
labgambino <113803192+labgambino@users.noreply.github.com>
2023-04-28 12:14:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function ir (){
+    
+var c=2244; 
+var u="inaser"; 
+    if(document.form.password.value==c && document.form.login.value==u){
+    alert ("INASER: Acceso permitido al EXPEDIENTE CONTR/2023/343294. \nTodos los Derechos reservados."); 
+window.location="hiperenlopag.html";} 
+    
+    else { 
+        alert("Por favor, ingresa tu usuario y contraseña correctos");
+    }
+     }
